659aa732476368ebdc1e5e1c0b1f09d7bb836e86
Nicholas Sharp <nsharp@cs.cmu.edu>
2021-05-28 17:44:19 -0400

    bump version [ci build]

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir='', exclude_arch=False):
