53a5a0ea09b2d86cd3e45f77204272aeba4af19d
Jarred Parr <jparr721@gmail.com>
2022-11-07 20:47:33 -0500

    add typing and some comments

--- src/potpourri3d/core.py
+++ src/potpourri3d/core.py
@@ -3,12 +3,35 @@ import potpourri3d_bindings as pp3db
 
 # Sanity checkers applied throughout
 
-def validate_mesh(V, F, force_triangular=False, test_indices=False):
+
+def validate_mesh(
+    V: np.ndarray,
+    F: np.ndarray,
+    force_triangular: bool = False,
+    test_indices: bool = False,
+) -> None:
+    """Validates a given surface mesh to ensure that the proper face and vertex
+    configurations are present.
+
+    Args:
+        V (np.ndarray): The vertex positions
+        F (np.ndarray): The faces
+        force_triangular (bool, optional): Whether or not to force the faces
+        to be triangular. Defaults to False.
+        test_indices (bool, optional): Whether or not to test if there are out
+        of bounds faces. Defaults to False.
+
+    Raises:
+        ValueError: Vertices are not a two-dimensional numpy array.
+        ValueError: Faces are not a two-dimensional numpy array.
+        ValueError: Faces are not triangular.
+        ValueError: There is an out of bounds face index.
+    """
 
     if len(V.shape) != 2 or V.shape[1] != 3:
         raise ValueError("vertices should be a 2d Nx3 numpy array")
     n_vert = V.shape[0]
-    
+
     if len(F.shape) != 2 or F.shape[1] < 3:
         raise ValueError("faces should be a 2d NxD numpy array, where D >= 3")
 
@@ -18,9 +41,12 @@ def validate_mesh(V, F, force_triangular=False, test_indices=False):
     if test_indices:
         max_elem = np.amin(F)
         if max_elem >= n_vert:
-            raise ValueError("There is an out-of-bounds face index. Faces should be zero-based array of indices to vertices")
+            raise ValueError(
+                "There is an out-of-bounds face index. Faces should be zero-based array of indices to vertices"
+            )
 
-def validate_points(V):
-    
+
+def validate_points(V: np.ndarray):
+
     if len(V.shape) != 2 or V.shape[1] != 3:
         raise ValueError("vertices should be a 2d Nx3 numpy array")
--- src/potpourri3d/mesh.py
+++ src/potpourri3d/mesh.py
@@ -1,79 +1,93 @@
+from typing import List, Tuple
+
 import numpy as np
 import potpourri3d_bindings as pp3db
-
 import scipy
 import scipy.sparse
 
 from .core import *
 
-class MeshHeatMethodDistanceSolver():
 
-    def __init__(self, V, F, t_coef=1., use_robust=True):
+class MeshHeatMethodDistanceSolver:
+    def __init__(
+        self, V: np.ndarray, F: np.ndarray, t_coef: float = 1.0, use_robust: bool = True
+    ) -> None:
         validate_mesh(V, F, force_triangular=True, test_indices=True)
         self.bound_solver = pp3db.MeshHeatMethodDistance(V, F, t_coef, use_robust)
 
-    def compute_distance(self, v_ind):
+    def compute_distance(self, v_ind: np.ndarray) -> float:
         return self.bound_solver.compute_distance(v_ind)
-    
-    def compute_distance_multisource(self, v_inds):
+
+    def compute_distance_multisource(self, v_inds: np.ndarray) -> float:
         return self.bound_solver.compute_distance_multisource(v_inds)
 
-def compute_distance(V, F, v_ind): 
+
+def compute_distance(V: np.ndarray, F: np.ndarray, v_ind: np.ndaaray) -> np.ndarray:
     solver = MeshHeatMethodDistanceSolver(V, F)
     return solver.compute_distance(v_ind)
 
-def compute_distance_multisource(V, F, v_inds):
+
+def compute_distance_multisource(
+    V: np.ndarray, F: np.ndarray, v_inds: np.ndarray
+) -> np.ndarray:
     solver = MeshHeatMethodDistanceSolver(V, F)
     return solver.compute_distance_multisource(v_inds)
 
 
-class MeshVectorHeatSolver():
-
-    def __init__(self, V, F, t_coef=1.):
+class MeshVectorHeatSolver:
+    def __init__(self, V: np.ndarray, F: np.ndarray, t_coef: float = 1.0):
         validate_mesh(V, F, force_triangular=True, test_indices=True)
         self.bound_solver = pp3db.MeshVectorHeatMethod(V, F, t_coef)
 
-    def extend_scalar(self, v_inds, values):
+    def extend_scalar(self, v_inds: np.ndarray, values: np.ndarray) -> np.ndarray:
         if len(v_inds) != len(values):
-            raise ValueError("source vertex indices and values array should be same shape")
+            raise ValueError(
+                "source vertex indices and values array should be same shape"
+            )
         return self.bound_solver.extend_scalar(v_inds, values)
-    
-    def get_tangent_frames(self):
+
+    def get_tangent_frames(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         return self.bound_solver.get_tangent_frames()
-    
-    def get_connection_laplacian(self):
+
+    def get_connection_laplacian(self) -> scipy.sparse.csr_matrix:
         return self.bound_solver.get_connection_laplacian()
-    
-    def transport_tangent_vector(self, v_ind, vector):
+
+    def transport_tangent_vector(self, v_ind: int, vector: np.ndarray) -> np.ndarray:
         if len(vector) != 2:
             raise ValueError("vector should be a 2D tangent vector")
         return self.bound_solver.transport_tangent_vector(v_ind, vector)
-    
-    def transport_tangent_vectors(self, v_inds, vectors):
+
+    def transport_tangent_vectors(
+        self, v_inds: np.ndarray, vectors: np.ndarray
+    ) -> np.ndarray:
         if len(v_inds) != len(vectors):
-            raise ValueError("source vertex indices and values array should be same length")
+            raise ValueError(
+                "source vertex indices and values array should be same length"
+            )
         return self.bound_solver.transport_tangent_vectors(v_inds, vectors)
-    
-    def compute_log_map(self, v_ind):
+
+    def compute_log_map(self, v_ind: int) -> np.ndarray:
         return self.bound_solver.compute_log_map(v_ind)
 
-class EdgeFlipGeodesicSolver():
 
-    def __init__(self, V, F, t_coef=1.):
+class EdgeFlipGeodesicSolver:
+    def __init__(self, V: np.ndarray, F: np.ndarray, t_coef: float = 1.0):
         validate_mesh(V, F, force_triangular=True)
         self.bound_solver = pp3db.EdgeFlipGeodesicsManager(V, F)
 
-    def find_geodesic_path(self, v_start, v_end):
+    def find_geodesic_path(self, v_start: int, v_end: int) -> np.ndarray:
         return self.bound_solver.find_geodesic_path(v_start, v_end)
-    
-    def find_geodesic_path_poly(self, v_list):
-        return self.bound_solver.find_geodesic_path_poly(v_list)
-    
-    def find_geodesic_loop(self, v_list):
-        return self.bound_solver.find_geodesic_loop(v_list)
-    
 
-def cotan_laplacian(V, F, denom_eps=0.):
+    def find_geodesic_path_poly(self, v_list: List[int]) -> np.ndarray:
+        return self.bound_solver.find_geodesic_path_poly(v_list)
+
+    def find_geodesic_loop(self, v_list: List[int]) -> np.ndarray:
+        return self.bound_solver.find_geodesic_loop(v_list)
+
+
+def cotan_laplacian(
+    V: np.ndarray, F: np.ndarray, denom_eps: float = 0.0
+) -> scipy.sparse.csr_matrix:
     validate_mesh(V, F, force_triangular=True)
     nV = V.shape[0]
 
@@ -81,15 +95,15 @@ def cotan_laplacian(V, F, denom_eps=0.):
     mat_j = []
     mat_data = []
     for i in range(3):
-      
+
         # Gather indices and compute cotan weight (via dot() / cross() formula)
-        inds_i = F[:,i]
-        inds_j = F[:,(i+1)%3]
-        inds_k = F[:,(i+2)%3]
-        vec_ki = V[inds_i,:] - V[inds_k,:]
-        vec_kj = V[inds_j,:] - V[inds_k,:]
+        inds_i = F[:, i]
+        inds_j = F[:, (i + 1) % 3]
+        inds_k = F[:, (i + 2) % 3]
+        vec_ki = V[inds_i, :] - V[inds_k, :]
+        vec_kj = V[inds_j, :] - V[inds_k, :]
         dots = np.sum(vec_ki * vec_kj, axis=1)
-        cross_mags =  np.linalg.norm(np.cross(vec_ki, vec_kj), axis=1)
+        cross_mags = np.linalg.norm(np.cross(vec_ki, vec_kj), axis=1)
         cotans = 0.5 * dots / (cross_mags + denom_eps)
 
         # Add the four matrix entries from this weight
@@ -97,15 +111,15 @@ def cotan_laplacian(V, F, denom_eps=0.):
         mat_i.append(inds_i)
         mat_j.append(inds_i)
         mat_data.append(cotans)
-        
+
         mat_i.append(inds_j)
         mat_j.append(inds_j)
         mat_data.append(cotans)
-        
+
         mat_i.append(inds_i)
         mat_j.append(inds_j)
         mat_data.append(-cotans)
-        
+
         mat_i.append(inds_j)
         mat_j.append(inds_i)
         mat_data.append(-cotans)
@@ -119,17 +133,37 @@ def cotan_laplacian(V, F, denom_eps=0.):
 
     return L_coo.tocsr()
 
-def face_areas(V, F):
+
+def face_areas(V: np.ndarray, F: np.ndarray) -> np.ndarray:
+    """Computes the areas spanned by each triangle in a surface mesh.
+
+    Args:
+        V (np.ndarray): The vertex positions.
+        F (np.ndarray): The face indices.
+
+    Returns:
+        np.ndarray: The face areas as a numpy array.
+    """
     validate_mesh(V, F, force_triangular=True)
 
-    vec_ij = V[F[:,1],:] - V[F[:,0],:]
-    vec_ik = V[F[:,2],:] - V[F[:,0],:]
+    vec_ij = V[F[:, 1], :] - V[F[:, 0], :]
+    vec_ik = V[F[:, 2], :] - V[F[:, 0], :]
 
     areas = 0.5 * np.linalg.norm(np.cross(vec_ij, vec_ik), axis=1)
-    
+
     return areas
 
-def vertex_areas(V, F):
+
+def vertex_areas(V: np.ndarray, F: np.ndarray) -> np.ndarray:
+    """Compute the vertex areas for a given surface mesh.
+
+    Args:
+        V (np.ndarray): The vertex positions.
+        F (np.ndarray): The face indices.
+
+    Returns:
+        np.ndarray: The vertex areas as a numpy array.
+    """
     validate_mesh(V, F, force_triangular=True)
     nV = V.shape[0]
 
@@ -137,7 +171,7 @@ def vertex_areas(V, F):
 
     vertex_area = np.zeros(V.shape[0])
     for i in range(3):
-        vertex_area += np.bincount(F[:,i], face_area, minlength=nV)
-    vertex_area /= 3.
-    
+        vertex_area += np.bincount(F[:, i], face_area, minlength=nV)
+    vertex_area /= 3.0
+
     return vertex_area
